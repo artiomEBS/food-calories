c588b48d627a26e588e731760dbb7ddc944a202a
alexjeman <contact@alexjeman.com>
2020-08-11 10:47:20 +0300

    Add user Profile and Target tables

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from apps.users.models import Profile, Target
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'gender', 'height', 'weight', 'date_of_birth')
+
+
+@admin.register(Target)
+class TargetAdmin(admin.ModelAdmin):
+    list_display = ('user', 'energy', 'protein', 'carbohydrate', 'fat', 'fiber', 'salt', 'sugar')
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.common.models import BaseModel
 
-# Create your models here.
+
+class Profile(BaseModel):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female')
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    date_of_birth = models.DateField()
+
+
+class Target(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    energy = models.IntegerField()
+    protein = models.DecimalField(max_digits=19, decimal_places=4)
+    carbohydrate = models.DecimalField(max_digits=19, decimal_places=4)
+    fat = models.DecimalField(max_digits=19, decimal_places=4)
+    fiber = models.DecimalField(max_digits=19, decimal_places=4)
+    salt = models.DecimalField(max_digits=19, decimal_places=4)
+    sugar = models.DecimalField(max_digits=19, decimal_places=4)
+
+
