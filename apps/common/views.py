dc2ff9cfa192d5ce9fc550ab2a3ae87c60065cbb
artiom <ordersone@gmail.com>
2020-08-25 14:43:17 +0300

    Views refactored little bit

---
+++ apps/common/views.py
@@ -0,0 +1,42 @@
+from django.db.models import Q
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
+
+from apps.common.permissions import IsOwner, HasAPIKey
+
+
+class BaseViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, HasAPIKey, IsOwner]
+    model = None
+    detail_serializer_class = None
+    create_serializer_class = None
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return self.detail_serializer_class
+
+        if self.action in ('create', 'update', 'partial_update'):
+            return self.create_serializer_class
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user.id)
+
+    def perform_create(self, serializer):
+        serializer.save(is_public=False, user=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(is_public=False, user=self.request.user)
+
+
+class SearchBaseViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    model = None
+    serializer_class = None
+    filterset_class = None
+
+    def get_queryset(self):
+        query = Q(is_public=True) | Q(user=self.request.user.id)
+        return self.model.objects.filter(query)
--- apps/journal/views.py
+++ apps/journal/views.py
@@ -1,40 +1,5 @@
-from rest_framework import viewsets
-from rest_framework.response import Response
-
-from rest_framework.permissions import IsAuthenticated
-from apps.common.permissions import IsOwner, HasAPIKey
-from apps.journal import models
-from apps.journal import serializers
-
-
-class BaseViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated, HasAPIKey, IsOwner]
-    model = None
-    detail_serializer_class = None
-    create_serializer_class = None
-
-    def get_serializer_class(self):
-        if self.action in ('list', 'retrieve'):
-            return self.detail_serializer_class
-
-        if self.action in ('create', 'update', 'partial_update'):
-            return self.create_serializer_class
-
-    def get_queryset(self):
-        return self.model.objects.filter(user=self.request.user.id)
-
-    def create(self, request, *args, **kwargs):
-        serializer_class = self.get_serializer_class()
-        serializer = serializer_class(data=request.data)
-
-        if not serializer.is_valid():
-            return Response(serializer.errors)
-
-        self.perform_create(serializer)
-        return Response(serializer.data)
-
-    def perform_create(self, serializer):
-        serializer.save(is_public=False, user=self.request.user)
+from apps.common.views import BaseViewSet
+from apps.journal import models, serializers
 
 
 class FoodJournalViewSet(BaseViewSet):
@@ -46,6 +11,9 @@ class FoodJournalViewSet(BaseViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
+
 
 class ActivityJournalViewSet(BaseViewSet):
     """ CRUD on user's ActivityJournal. Queryset: user=request.user """
@@ -54,4 +22,7 @@ class ActivityJournalViewSet(BaseViewSet):
     create_serializer_class = serializers.ActivityJournalCreateSerializer
 
     def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
         serializer.save(user=self.request.user)
